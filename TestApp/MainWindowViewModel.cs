f62f3eecf9226aacba88698e2c5f2c7e6adaba73
439440190 <33275435+439440190@users.noreply.github.com>
2022-08-22 18:55:08 +0800

    20220822.1

---
+++ TestApp/MainWindowViewModel.cs
@@ -0,0 +1,65 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp
+{
+    internal class MainWindowViewModel: ObservableObject
+    {
+        public MainWindowViewModel()
+        {
+            CalculateCurrentCommand = new RelayCommand(CalculateCurrent);
+        }
+        #region 计算电流
+
+        //额定功率
+        private string pe;
+        public string Pe
+        {
+            get => pe;
+            set => SetProperty(ref pe, value);
+
+        }
+
+        //需要系数
+        private string kx;
+        public string Kx
+        {
+            get => kx;
+            set => SetProperty(ref kx, value);
+        }
+
+        //功率因数
+        private string cosø;
+        public string Cosø
+        {
+            get => cosø;
+            set => SetProperty(ref cosø, value);
+        }
+
+        //计算电流
+        private string ic;
+        public string Ic
+        {
+            get => ic;
+            set => SetProperty(ref ic, value);
+        }
+
+        
+        public IRelayCommand CalculateCurrentCommand { get;  }
+
+        void CalculateCurrent()
+        {
+            if (string.IsNullOrEmpty(pe) || string.IsNullOrEmpty(kx) || string.IsNullOrEmpty(cosø)) return;
+
+            Ic = Math.Round((double.Parse(pe) * double.Parse(kx)) / (Math.Sqrt(3) * double.Parse(cosø)), 2).ToString();
+        }
+
+        #endregion
+    }
+}
--- TimeIsLife/02 ViewModel/ElectricalViewModel.cs
+++ TimeIsLife/02 ViewModel/ElectricalViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 
 using System;
 using System.Collections.Generic;
@@ -8,45 +9,53 @@ using System.Threading.Tasks;
 
 namespace TimeIsLife.ViewModel
 {
-    class ElectricalViewModel
+    class ElectricalViewModel:ObservableObject
     {
         #region 计算电流
 
         //额定功率
-        [ObservableProperty]
-        [NotifyPropertyChangedFor(nameof(Pc))]
-        public string pe;
-
-        //需要系数
-        [ObservableProperty]
-        [NotifyPropertyChangedFor(nameof(Pc))]
-        public string kx;
-
-        //计算有功功率
-        public string Pc 
+        private string pe;
+        public string Pe
         {
-            get => $"{pe}{kx}"; 
+            get => pe;
+            set => SetProperty(ref pe, value);
+
         }
 
-        //计算无功功率
-        public string Qc { get; set; }
+        //需要系数
+        private string kx;
+        public string Kx
+        {
+            get => kx;
+            set => SetProperty(ref kx, value);
+        }
 
         //功率因数
-        [ObservableProperty]
-        public string cosø;
+        private string cosø;
+        public string Cosø
+        {
+            get => cosø;
+            set => SetProperty(ref cosø, value);
+        }
 
         //计算电流
-        [ObservableProperty]
-        public string ic;
+        private string ic;
+        public string Ic
+        {
+            get => ic;
+            set => SetProperty(ref ic, value);
+        }
 
-        [ObservableProperty]
-        public string lj;
+        private RelayCommand greetUserCommand;
 
-        [ObservableProperty]
-        public string ln;
+        public IRelayCommand GreetUserCommand => greetUserCommand ??= new RelayCommand(CalculateCurrent);
 
+        void CalculateCurrent()
+        {
+            if (string.IsNullOrEmpty(pe) || string.IsNullOrEmpty(kx) || string.IsNullOrEmpty(cosø)) return;
 
-
+            ic = Math.Round((double.Parse(pe) * double.Parse(kx)) / (Math.Sqrt(3) * double.Parse(cosø)), 2).ToString();
+        }
         #endregion
     }
 }
